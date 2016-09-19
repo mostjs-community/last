79f1fa076ede07b5026503bf0a7dbe9394997bff
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-09-19 14:46:12 -0400

    chore(most-last): generate sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import buba from 'rollup-plugin-buba'
 
 export default {
-  entry: "src/index.js",
-  dest: "dist/most-last.js",
-  moduleName: "mostLast",
-  format: "umd",
+  entry: 'src/index.js',
+  dest: 'dist/most-last.js',
+  moduleName: 'mostLast',
+  format: 'umd',
+  sourceMap: true,
   plugins: [ buba() ]
 }
